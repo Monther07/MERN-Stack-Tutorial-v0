4b6a74b5d8567539161ecfda82960245c17bf6d4
MontherG <dev.monther@gmail.com>
2022-08-06 20:59:44 +0300

    add Route

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,11 @@ const express = require('express')
 // express app
 const app = express()
 
+// route
+app.get('/', (req, res) => {
+  res.json({ mssg: 'Welcome to he app' })
+})
+
 // listen for requst
 app.listen(4000, () => {
   console.log('Listining to the port 4000')
