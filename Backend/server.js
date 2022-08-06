af105b28571d830dcd17176301721ab9e2a26cea
MontherG <dev.monther@gmail.com>
2022-08-06 22:14:41 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,15 @@
-// to let the dotenv work on the project 
+// to let the dotenv work on the project
 require('dotenv').config()
 const express = require('express')
 
 // express app
 const app = express()
 
+// middelware
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
 // route
 app.get('/', (req, res) => {
   res.json({ mssg: 'Welcome to the app' })
