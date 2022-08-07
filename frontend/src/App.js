cf334f5de4739d0fcea8df54598fec311fff0a0d
MontherG <dev.monther@gmail.com>
2022-08-08 00:07:42 +0300

    ش.ك npm init @eslint/config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom '
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-// pages & componots
+// pages & components
 import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   )
